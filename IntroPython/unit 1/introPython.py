a2d43906dcdd0a799ceb2546aae0318495923a0b
Ian <38480036+Iankimble@users.noreply.github.com>
2025-02-05 19:22:29 +0000

    learned about variables

--- IntroPython/unit 1/introPython.py
+++ IntroPython/unit 1/introPython.py
@@ -31,7 +31,7 @@
 
 # we dont need to wrap numbers up in quotes.
 # we can just write them normally.
-
+678687658758 
 12
 123
 3488
@@ -41,7 +41,8 @@
 # it is considered a float data type.
 12.03
 23.56
-
+67.98
+809.09
 
 # Boolean Data Type 
 # A data representation of true and false
@@ -51,3 +52,21 @@ False
 
 True
 1 # represent true 
+
+# Variable is a type of container or mask for data.
+# variable values can change but the variable name will stay the same.
+
+userName = 'Ian'
+emailAdress = 'ian@gmial.com'
+profilePicture = 'profile.png'
+phoneNumber = 21239384
+
+# the input function is used for passing in data. 
+# whatever we type into the input will take on the value of the
+# variable, which in this case is name
+name = input('what is your name? ')
+
+print(name)
+
+# the print function is for passing data out from your program
+print('hello from Python')
