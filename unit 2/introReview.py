4ab017fac142bae6a9803bbdbee2483b690a6b92
Ian <38480036+Iankimble@users.noreply.github.com>
2025-03-06 13:10:47 +0000

    python intro review question

---
+++ unit 2/introReview.py
@@ -0,0 +1,23 @@
+# 1. Fix the function below so that it will 
+# print out the age value properly.
+def printAge(age):
+    print('I am '+ age + ' years old.')
+
+printAge(10)
+
+# 2. Fix the function below so that the 
+# when it will print the result value properly.
+def multiplyValues():
+    num1 = input('Please enter a number: ')
+    num2 = input('Please enter another number: ')
+    print(num1 + num2)    
+
+# 3. Create a function that will 
+# count the number of characters in a string. 
+# You will need to use the Python count() function 
+# to accomplish this. 
+
+# 4. Create a function that will convert hours into minutes. 
+# Your function should take in 1 parameter that will represent 
+# the hour and your function should calculate and return 
+# how many minutes are in that amount of time. 
