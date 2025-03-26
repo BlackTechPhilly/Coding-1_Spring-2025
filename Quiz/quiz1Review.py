5da2329bb774f196c6dff8f2cc8bba7e8423a4b9
Ian <38480036+Iankimble@users.noreply.github.com>
2025-03-26 18:20:06 +0000

    quiz review answers

---
+++ Quiz/quiz1Review.py
@@ -0,0 +1,116 @@
+# ## Coding 1 Spring Quiz
+# Create a new document called 
+# <b>SpringQuiz1.py</b> inside of your unit 2 folder. 
+
+# Next, answer the follwing questions. 
+# When you have completed your quiz, pleas 
+# <b>make sure you commit and sync your work in order to get credit.</b>
+
+# This exam will be graded on effort AND accuracy. If you made a genuine attempt at answering the question but your answer was incorrect or incomplete, you will recieve partial credit. If you answered the question correctly and completely you will receive full credit.
+
+# ## Quiz rules
+# 1. This quiz is open note and open web,
+#  meaning you may use your notes from previous
+#  classes as well as websites such as W3schools 
+# to help you answer the questions.
+
+# 2. You may not use any LLM or AI websites for this quiz. If you are caught with any of these apps open you will receive an initial warning. If you are caught again you will automatically fail the quiz. 
+
+# 3. No phones should be out during the quiz. If you are caught with your phone out you will receive an initial warning. If the phone is still out
+# after being warned, it will be confiscated until the end of the day.
+
+# 4. No music is allowed during the quiz. Out of respect for your peers, 
+# please refrain from playing any music while others are taking the quiz as to not distract them.
+
+# Good Luck! 
+
+# ## Quiz Questions
+
+# 1. What is the difference between a integer 
+# data type and a float data type? 
+# Please write your answer in a 
+# complete sentence as a string data type.
+
+" A float is a number with a decimal."
+" A integer is a whole number/ non-decimal."
+
+# 2. What will the following code expression output?
+
+result = 10 / 3
+print(result)
+'the answer is 3.33~'
+
+# Please write your answer in the form of a string.
+
+# 3. What will be the output of the following code? 
+# Then, Explain what is happening in a complete 
+# sentence. Please write your response as a 
+# string data type.
+
+# python
+
+num = "25"
+new_num = int(num) + 5
+print(new_num)
+
+'The first variable 25 is '
+'being added to the number 5 in the'
+'new num variable and then it is '
+'printing the result which 30'
+
+# 4. Create a function that asks a user for 
+# their birth year using the input() function and 
+# then calculates their age.
+# * hint:  you will need to use type casting to 
+# change the data type of your input.
+
+def ageCalculator():
+    response = input('What is your birth year? ')
+    age = 2025- int(response)
+    print(age)
+
+# ageCalculator()
+
+# 5. What is the difference between a parameter and 
+# an argument in a function? Provide an example. 
+# Please write your answer in complete sentences as 
+# a string data type. 
+
+' parameter is placeholder data '
+' that goes in the function definition'
+
+' argument is real data tht goes '
+' into the function call.'
+
+def sandwichTray(orderCount):
+    # ingredients
+    # condiments
+    # packaging 
+    # napkins
+    print('you will get ' + orderCount + ' sandwich trays')
+#sandwichTray(3)
+
+
+# <b>6. Take what you've learned about functions, 
+# data types, and operators
+# to answer the following question: </b> 
+
+# You have been hired by Target to develop a 
+# gift card kiosk app that will alow users to 
+# reload their gift cards by a set amount. 
+# Your program should ask the user to enter 
+# a gift card amount followed by the 
+# amount they want to add to it. Next, your 
+# program should calculate the new amount by 
+# adding the new added amount to the old balance. 
+# Finally, your program should print out a 
+# message telling the user "congrats! 
+# Your new balance is (the new total) dollars. 
+
+def giftCardUpdate():
+    currentBalance = input('how much is on your gift card? ')
+    updateAmount = input('how much do you want to add? ')
+    newBalance = int(currentBalance) + int(updateAmount)
+    print(' you have ' + str(newBalance) + " on card" )
+
+giftCardUpdate()
