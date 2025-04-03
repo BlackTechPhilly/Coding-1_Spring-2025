6faf9cd814e870c03c5be1aa78094fa39f3718fe
Ian <38480036+Iankimble@users.noreply.github.com>
2025-04-03 17:49:37 +0000

    loop problem

---
+++ unit 4/loops.py
@@ -0,0 +1,33 @@
+# While Loops are a type of loop that will
+# keep the code running so long as the 
+# looping condition is true. 
+
+endOfSchool ='April 2nd'
+
+#while endOfSchool !='May 31st':
+ #   print('school is in session.')
+
+dismissalHour = 3
+while dismissalHour != 3:
+    print('school is in session.')
+
+# 1 kosher menu
+# 2 vegitarian menu
+# 3 allergy menu
+# 4 lactose intolerant menu
+# 5 nothing  menu
+
+foodOrder = 1
+
+while foodOrder == 1:
+    print('You will get the kosher option')
+
+# Create a simple count-down timer using a while loop
+# you code should count down from 10 and stop at zero.
+# - HINT: you will need to use either assignment or arithmatic operators
+# to make the numbers count down. 
+
+
+
+
+# Create a simple multiplication table
